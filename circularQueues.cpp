0404af5051a7fcf4d14aa82981fd339ef2fbb391
feraistanti <fistanti551@gmail.com>
2024-05-31 15:19:23 +0700

    membuat int main

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -86,4 +86,33 @@ public:
 int main() {
     Queues q;
     char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement insert operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch) {
+            case '1': {
+
+                q.insert();
+                break;
+            }
+            case '2': {
+                q.remove();
+                break;
+            }
+            case '3': {
+                q.display();
+                break;
+            }
+            }
+        }
+    }
 }
