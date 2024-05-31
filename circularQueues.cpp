718c14306939ecc9831c4ff64692554b453caeb5
feraistanti <fistanti551@gmail.com>
2024-05-31 15:35:21 +0700

    menambahkan case 4

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -112,7 +112,9 @@ int main() {
                 q.display();
                 break;
             }
-            
+            case '4': {
+                return 0;
+            }
             default: {
                 cout << "Invalid option!!" << endl;
                 break;
