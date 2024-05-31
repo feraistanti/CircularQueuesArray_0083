90ad8d9d9479d3220bc60bac175ff6dfcc813b20
feraistanti <fistanti551@gmail.com>
2024-05-31 14:42:47 +0700

    mengecek antrian yang kosong

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -18,9 +18,23 @@ public:
         cout << endl;
 
         // cek apakah antrian penuh
-        if ((FRRONT == 0 && REAR == max -1) || (FRONT == REAR + 1)) {
+        if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1)) {
             cout << "\nQueue overflow\n";
             return;
         }
+
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            //Jika Rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] num;
     }
 }
